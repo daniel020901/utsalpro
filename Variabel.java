c33946ad33f993cd2ef1dbe9c7055895376c09db
muhammad daniel ilyasa <115854547+daniel020901@users.noreply.github.com>
2022-11-17 20:05:59 +0700

    Add files via upload

---
+++ Variabel.java
@@ -0,0 +1,19 @@
+
+public class Variabel {
+
+	public static void main(String[] args) {
+		System.out.println("assalamualaikum");
+		
+		 String firstName = " Nama saya: ";
+		    String lastName = "Muhammad daniel ilyasa";
+		    String fullName = firstName + lastName;
+		    System.out.println(fullName); 
+		    
+		    int tahun= 2001;
+		    System.out.println("Saya lahir di tahun:" +tahun);
+		 
+		
+
+	}
+
+}
